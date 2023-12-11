e874021d476c88d5c80dcb7913f0225931670643
manikantakailasa <manikanta.kailasa@gmail.com>
2023-12-11 21:56:12 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Landing from "./components/landing/Landing";
-import WhatWeDo from "./components/whatwedo/WhatWeDo";
-import Driver from "./components/driver/Driver";
-import Luxury from "./components/luxury/Luxury";
 import Footer from "./components/footer/Footer";
-import Agile from "./components/whatwedo/Agile";
-import Clients from "./components/Clients/Clients";
-import Ourservice from "./components/ourservice/Ourservice";
-import Approach from "./components/approach/Approach";
-import Numbers from "./components/numbers/Numbers";
-import Aboutus from "./components/aboutus/Aboutus";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLanding from "./components/mainLanding/MainLanding";
 import Contactus from "./components/contactus/Contactus";
@@ -25,9 +16,9 @@ function App() {
       >
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<MainLanding />}></Route>
-          <Route path="/source" element={<Landing />}></Route>
-          <Route path="/contact" element={<Contactus />}></Route>
+          <Route exact path="./" element={<MainLanding />}></Route>
+          <Route path="./source" element={<Landing />}></Route>
+          <Route path="./contact" element={<Contactus />}></Route>
         </Routes>
 
         {/* <Agile />
